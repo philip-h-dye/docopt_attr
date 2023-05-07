ec737e2a4722d51cdfec2e3c0c949562a07b23a9
phdye <phdye@acm.org>
2023-05-07 17:25:29 -0400

    Updated and generalized

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-from setuptools import setup, find_packages
-from setuptools.command.install import install
+from setuptools import setup
+from pathlib import Path
+import re
 
-from src.version import __version__
+pypi_name   = 'docopt-attr'
+package     = pypi_name.replace('-', '_')
+version     = re.search("^__version__\s*=\s*'(.*)'",
+                        Path(f'{package}/__init__.py').read_text(),
+                        re.M ).group(1)
 
-from pprint import PrettyPrinter
-pp = PrettyPrinter(indent=4).pprint
-
-setup(
-    name='docopt_attr',
-    version=__version__,
-    description="docopt with options as attributes rather than dictionary elements",
-    author='Philip H. Dye',
-    author_email='philip@phd-solutions.com',
-    packages=find_packages(exclude=['tests', 'src', 's', 'log']),
-    url='https://github.com/phdye/docopt_attr',
-    requires=['docopt'],
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
+setup (
+    name = pypi_name,
+    version = version,
+    description = f'docopt with options as attributes rather than dictionary elements'
+    author = 'Philip H. Dye',
+    author_email = 'philip@acm.org',
+    url = f'https://www.github.com/philip-d-dye/{pypi_name}',
+    long_description = Path('README.txt').read_text(),
+    packages = (pypi_name),
+    license_files = ( 'LICENSE' ),
+    install_requires = ( 'docopt',),
+    tests_require=('pytest','pytest-runner'),
+    entry_points = {
+        'console_scripts': ( f'{command} = {package}:main',
+                           ) },
 )
-
-#
