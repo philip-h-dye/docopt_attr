ca37a789afaa6cbb3b9719431a9873fee4c5e9aa
phdye <phdye@acm.org>
2023-05-07 21:53:57 -0400

    cleanup

--- docopt_attr/api.py
+++ docopt_attr/api.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 
 from docopt import docopt
 
 from collections.abc import Mapping
 
-#------------------------------------------------------------------------------
 
 class AttributeConfig(Mapping):
 
     def __init__(self, *args, **kwargs):
-        # self.attr = [ ]
         for key, value in kwargs.items() :
             setattr(self, key, value)
-            # self.attr.append(key)
 
     def __getitem__(self, key):
         return self.__dict__[key]
@@ -33,7 +31,6 @@ class AttributeConfig(Mapping):
     def __repr__(self):
         return self.__str__()
 
-#------------------------------------------------------------------------------
 
 def docopt_attr(doc, argv=None, help=True, version=None, options_first=False):
 
@@ -56,6 +53,7 @@ def docopt_attr(doc, argv=None, help=True, version=None, options_first=False):
     clean = { '_docopt' : args }
 
     for key in args :
+        original_key = key
         value = args[key]
         if key.startswith('<') and key.endswith('>') :
             key = key[1:][:-1]
@@ -67,13 +65,21 @@ def docopt_attr(doc, argv=None, help=True, version=None, options_first=False):
             key = key[1:]
             # print(f": -- {key:<16s} : {repr(value)}")
         key = key.replace('-', '_')
+
+        # Replace any remaining non-identifier characters with underscore
         if not key.isidentifier() :
-            raise ValueError("Cleaned key '{key}' is not an valid identifier.\n"
-                             "This is a docopt_attr internal error.  Please report this to the maintainer.")
+            key = re.sub(r'[^A-Za-z_]','_',key)
+        if not key.isidentifier() :
+            raise ValueError(f"Cleaned '{key}' is not an valid identifier.\n"
+                             f"This occurred when converting '{original_key}'.\n"
+                             f"This is a docopt_attr internal error.\n"
+                             f"Please report this to the maintainer at github.")
+        if key in clean :
+            raise ValueError(f"Duplicate key '{key}' found converting '{original_key}' "
+                             f"to an attribute.\nPlease adjust your docstring to resolve.")
+
         clean [ key ] = value
 
     return AttributeConfig(**clean)
         
 docopt_attr.__doc__ = docopt_attr.__doc__.replace('<docopt-doc>', docopt.__doc__ )
-
-# ------------------------------------------------------------------------------
