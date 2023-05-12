bdf4b83485a5dfa9e974d5ffce58fe60c22e26e2
phdye <phdye@acm.org>
2023-05-12 05:57:32 -0400

    Move package down into src

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from pathlib import Path
 import re
 
 pypi_name   = 'docopt-attr'
+
 package     = pypi_name.replace('-', '_')
+
 version     = re.search("^__version__\s*=\s*'(.*)'",
-                        Path(f'{package}/__init__.py').read_text(),
+                        Path(f'src/{package}/__init__.py').read_text(),
                         re.M ).group(1)
 
 setup (
@@ -15,9 +17,11 @@ setup (
     author = 'Philip H. Dye',
     author_email = 'philip@acm.org',
     url = f'https://www.github.com/philip-d-dye/{pypi_name}',
-    long_description = Path('README.txt').read_text(),
-    packages = (package,),
-    license_files = ( 'LICENSE', ),
-    install_requires = ( 'docopt',),
-    tests_require=('pytest','pytest-runner'),
+    long_description = Path('README.md').read_text(),
+    packages = (package, ),
+    package_dir={'' : 'src'},
+    license_files = ('LICENSE', ),
+    install_requires = ('docopt', ),
+    tests_require=('pytest', ),
 )
+
